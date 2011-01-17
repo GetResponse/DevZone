b6fb0d6c81aeec49222b09f550b1aaf8129b42ff
Pawel Lugowski <plugowski@implix.com>
2011-01-17 08:18:32 +0100

    Notifications language choise

--- Plugins/Joomla/source/mod_getresponse.php
+++ Plugins/Joomla/source/mod_getresponse.php
@@ -4,6 +4,8 @@
 
     $wid = $params->get('webform_id');
     $css = $params->get('display_css') == 'yes' ? '' : '&css=1';
+	$domain = $params->get('notify_language');
+	$domain = !empty($domain) ? $domain : 'com';
 
-    echo '<script type="text/javascript" src="http://www.getresponse.pl/view_webform.js?wid='.(int)$wid.$css.'"></script>';
+    echo '<script type="text/javascript" src="http://www.getresponse.'.$domain.'/view_webform.js?wid='.(int)$wid.$css.'"></script>';
 ?>
