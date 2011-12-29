d30bff6b31e218edca633e388048d9986613afc6
Pawel Pabian <bbkr@post.pl>
2011-12-29 07:19:26 -0500

    Fixed Content-Type header.

--- API/lib/jsonRPCClient.php
+++ API/lib/jsonRPCClient.php
@@ -132,7 +132,7 @@ class jsonRPCClient
         curl_setopt($ch, CURLOPT_URL, $this->url);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
-        curl_setopt($ch, CURLOPT_HEADER, 'Content-type: application/json;');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
         curl_setopt($ch, CURLOPT_ENCODING, 'gzip,deflate');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
