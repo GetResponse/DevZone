3e00f3f9e1fda80c541efae9ff7841cc1cbbc6fa
Pawel Pabian <bbkr@post.pl>
2012-12-06 12:52:09 +0100

    refreshed Perl example
    Perl 5 module JSON::RPC::Client from CPAN is not compatible with 1.0 or
    2.0 spec (it uses legacy 1.1 draft) so Perl 6 is used.

--- API/examples/perl_synopsis.pl
+++ API/examples/perl_synopsis.pl
@@ -1,102 +1,65 @@
-#!/usr/bin/perl
+use v6;
 
-use strict;
-use warnings;
-
-=head1 DESCRIPTION
-
-Implementation of sample scenario using GetResponse API:
-
-Add new contact to campaign 'sample_marketing'.
-Start his follow-up cycle and set custom field
-'last_purchased_product' to 'netbook'.
-
-=head1 AUTHOR
-
-Pawel Pabian
-http://implix.com
-http://dev.getresponse.com
-
-=cut
-
-use Data::Dumper;
+# Demonstrates how to add new contact to campaign.
 
 # JSON::RPC module is required
-# available at http://search.cpan.org/perldoc?JSON%3A%3ARPC
+# available at https://github.com/bbkr/jsonrpc
+# or included in Rakudo Star distribution from https://github.com/rakudo/star
 use JSON::RPC::Client;
 
-# your API key
-# available at http://www.getresponse.com/my_api_key.html
+# your API key is available at
+# https://app.getresponse.com/my_api_key.html
 my $api_key = 'ENTER_YOUR_API_KEY_HERE';
 
 # API 2.x URL
 my $api_url = 'http://api2.getresponse.com';
 
 # initialize JSON-RPC client
-my $client = JSON::RPC::Client->new();
+my $client = JSON::RPC::Client.new( url => $api_url );
 
-# get CAMPAIGN_ID of 'sample_marketing' campaign
-my $response = $client->call(
-    $api_url,
-    {   'method' => 'get_campaigns',
-        'params' => [
-            $api_key,
-            {
+# find campaign named 'test'
+my $campaigns = $client.get_campaigns(
+    $api_key,
+    {
+        # find by name literally
+        'name' => {'EQUALS' => 'test'}
+    }
+);
 
-                # find by name literally
-                'name' => {'EQUALS' => 'sample_marketing'}
+# uncomment this line to preview Response
+# note $campaigns.perl;
+
+# because there can be only one campaign of this name
+# first key is the CAMPAIGN_ID required by next method
+# (this ID is constant and should be cached for future use)
+my $CAMPAIGN_ID = $campaigns.keys.pop;
+
+# add contact to the campaign
+my $response = $client.add_contact(
+    $api_key,
+    {
+        # identifier of 'test' campaign
+        'campaign' => $CAMPAIGN_ID,
+
+        # basic info
+        'name'  => 'Test',
+        'email' => 'test@test.com',
+
+        # custom fields
+        'customs' => [
+            {   'name'    => 'likes_to_drink',
+                'content' => 'tea'
+            },
+            {   'name'    => 'likes_to_eat',
+                'content' => 'steak'
             }
         ]
     }
 );
 
-# check for communication and response errors
-# implement handling if needed
-die $client->status_line unless $response;
-die $response->error_message if $response->is_error;
+# uncomment this line to preview Response
+# note $response.perl;
 
-# uncomment this line to preview data structure
-# print Dumper $response->result;
+say 'Contact added';
 
-# since there can be only one campaign of this name
-# first key is the CAMPAIGN_ID you need
-my $CAMPAIGN_ID = [keys %{$response->result}]->[0];
-
-# add contact to 'sample_marketing' campaign
-$response = $client->call(
-    $api_url,
-    {   'method' => 'add_contact',
-        'params' => [
-            $api_key,
-            {
-
-                # campaign to which contact should be added
-                'campaign' => $CAMPAIGN_ID,
-
-                # basic required info
-                'name'  => 'Sample Name',
-                'email' => 'sample@email.com',
-
-                # contact should begin follow-up cycle
-                'cycle_day' => '0',
-
-                # custom field
-                'customs' => [
-                    {   'name'    => 'last_purchased_product',
-                        'content' => 'netbook'
-                    },
-                ]
-            }
-        ]
-    }
-);
-
-# check for communication and response errors
-# implement handling if needed
-die $client->status_line unless $response;
-die $response->error_message if $response->is_error;
-
-# uncomment this line to preview data structure
-# print Dumper $response->result;
-
-print "Contact added\n";
+# Pawel Pabian http://implix.com
