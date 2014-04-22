efe57645894a6ad6d440415984361ebd1ee1be07
Marek Gańko <ganko.marek@gmail.com>
2014-04-22 22:53:18 +0200

    Fixed getJsonLastErrorMsg for PHP5.5+

--- API/lib/jsonRPCClient.php
+++ API/lib/jsonRPCClient.php
@@ -136,7 +136,7 @@ class jsonRPCClient
      */
     public function __call($pMethod, $pParams)
     {
-        static $requestId = 1;
+        static $requestId = 0;
 
         // generating uniuqe id per process
         $requestId++;
@@ -298,17 +298,17 @@ class jsonRPCClient
             function json_last_error_msg()
             {
                 static $errors = array(
-                    JSON_ERROR_NONE           => null,
+                    JSON_ERROR_NONE           => 'No error',
                     JSON_ERROR_DEPTH          => 'Maximum stack depth exceeded',
                     JSON_ERROR_STATE_MISMATCH => 'Underflow or the modes mismatch',
                     JSON_ERROR_CTRL_CHAR      => 'Unexpected control character found',
-                    JSON_ERROR_SYNTAX         => 'Syntax error, malformed JSON',
+                    JSON_ERROR_SYNTAX         => 'Syntax error',
                     JSON_ERROR_UTF8           => 'Malformed UTF-8 characters, possibly incorrectly encoded'
                 );
                 $error = json_last_error();
                 return array_key_exists($error, $errors) ? $errors[$error] : 'Unknown error (' . $error . ')';
             }
         }
-        return json_last_error_msg();
+        return json_last_error() ? json_last_error_msg() : null;
     }
 }
